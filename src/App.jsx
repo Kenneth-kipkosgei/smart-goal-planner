000a279de49bde559a619faf2dbb905f37eef701
Kenneth-kipkosgei <kennethkosgei38@gmail.com>
2025-07-27 18:49:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Overview from './components/Overview.jsx';
 import GoalList from './components/GoalList.jsx';
 import AddGoalForm from './components/GoalForm.jsx';
 
-const API_URL = "http://localhost:3001/goals";
+const API_URL = "https://smart-goal-api-70zr.onrender.com/goals"; 
 
 function App() {
   const [goals, setGoals] = useState([]);
